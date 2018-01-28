b0159b4d0257316c02e41c626b135198cf237e1d
moh <dabiri1377@gmail.com>
2018-01-28 15:37:42 +0330

    add some test for 1.2.3

---
+++ 1.2.3_test_1.py
@@ -0,0 +1,67 @@
+# ali is an id for bool class
+ali = bool()
+# print type of ali
+print("type of ali = ", end='')
+print(type(ali))
+# print value of ali
+print("value of ali =", end='')
+print(ali)
+
+# ali is id for True, in the bool class
+ali = True
+# print type of ali
+print("type of ali = ", end='')
+print(type(ali))
+# print value of ali
+print("value of ali =", end='')
+print(ali)
+
+# ali_2 is ID for True
+ali_2 = True
+# print bool value of ali_2
+print(bool(ali_2))
+
+# ali_3 is ID for string
+ali_3 = "sdfsadf"
+print(ali_3)
+# print type of ali_3
+print("type of ali_3 = ", end='')
+print(type(ali_3))
+# print bool value of ali_3
+print(bool(ali_3))
+
+# moh is an int
+moh = 12
+# print type of moh
+print("type of moh = ", end='')
+print(type(moh))
+# print value of moh
+print("value of moh =", end='')
+print(moh)
+
+# moh_2 is an int
+moh_2 = 12000000000000000000000020022
+# print type of moh_2
+print("type of moh_2 = ", end='')
+print(type(moh_2))
+# print value of moh_2
+print("value of moh_2 =", end='')
+print(moh_2)
+
+# moh_3 is an int, but Hex
+moh_3 = 0x23
+# print type of moh_3
+print("type of moh_3 = ", end='')
+print(type(moh_3))
+# print value of moh_3,base 10 value not Hex value
+print("value of moh_3 =", end='')
+print(moh_3)
+
+#####
+# in int:
+# for Hex   => 0x
+# for Oct   => 0o
+# for Bin   => 0b
+# for base 10 just type it
+
+
