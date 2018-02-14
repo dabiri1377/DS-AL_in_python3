334d72fc6cf7ca4c0d970e2d34ec30980af7cdcb
moh <dabiri1377@gmail.com>
2018-02-14 05:51:36 +0330

    add 'not', 'and', 'or' into 1.3_test.py

---
+++ Chapter_1/1.3_test.py
@@ -0,0 +1,42 @@
+# Expression, Operation and Precedence
+
+# Logical Operation
+# for boolean value
+bool_f = False
+bool_t = True
+# not
+print("not bool_f =", end='')
+print(not bool_f, end='\n\n')
+
+print("not bool_t =", end='')
+print(not bool_t, end='\n\n')
+
+# and
+print("bool_f and bool_f = ", end='')
+print(bool_f and bool_f, end='\n\n')
+
+print("bool_f and bool_t = ", end='')
+print(bool_f and bool_t, end='\n\n')
+
+print("bool_t and bool_f = ", end='')
+print(bool_t and bool_f, end='\n\n')
+
+print("bool_t and bool_t = ", end='')
+print(bool_t and bool_t, end='\n\n')
+
+# or
+print("bool_f or bool_f = ", end='')
+print(bool_f or bool_f, end='\n\n')
+
+print("bool_f or bool_t = ", end='')
+print(bool_f or bool_t, end='\n\n')
+
+print("bool_t or bool_f = ", end='')
+print(bool_t or bool_f, end='\n\n')
+
+print("bool_t or bool_t = ", end='')
+print(bool_t or bool_t, end='\n\n')
+
+
+
+
