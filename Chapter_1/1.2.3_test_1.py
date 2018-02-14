183aa543dbade51c3132cb5ba2c45985f2edd67f
moh <dabiri1377@gmail.com>
2018-02-14 04:58:32 +0330

    add a func. to 1.2.3_test_1.py and reformat everything

--- Chapter_1/1.2.3_test_1.py
+++ Chapter_1/1.2.3_test_1.py
@@ -1,61 +1,39 @@
+def print_type_value(datatype, name_of_var):
+    # print type of 'datatype'
+    print("type of " + str(name_of_var) + " = ", end='')
+    print(type(datatype))
+    # print value of datatype
+    print("value of " + str(name_of_var) + " =", end='')
+    print(datatype, end='\n\n')
+
+
 # ali is an id for bool class
 ali = bool()
-# print type of ali
-print("type of ali = ", end='')
-print(type(ali))
-# print value of ali
-print("value of ali =", end='')
-print(ali)
+print_type_value(ali, "ali")
 
 # ali is id for True, in the bool class
 ali = True
-# print type of ali
-print("type of ali = ", end='')
-print(type(ali))
-# print value of ali
-print("value of ali =", end='')
-print(ali)
+print_type_value(ali, "ali")
 
 # ali_2 is ID for True
 ali_2 = True
-# print bool value of ali_2
-print(bool(ali_2))
+print_type_value(ali_2, "ali_2")
 
 # ali_3 is ID for string
 ali_3 = "sdfsadf"
-print(ali_3)
-# print type of ali_3
-print("type of ali_3 = ", end='')
-print(type(ali_3))
-# print bool value of ali_3
-print(bool(ali_3))
+print_type_value(ali_3, "ali_3")
 
 # moh is an int
 moh = 12
-# print type of moh
-print("type of moh = ", end='')
-print(type(moh))
-# print value of moh
-print("value of moh =", end='')
-print(moh)
+print_type_value(moh, "moh")
 
 # moh_2 is an int
 moh_2 = 12000000000000000000000020022
-# print type of moh_2
-print("type of moh_2 = ", end='')
-print(type(moh_2))
-# print value of moh_2
-print("value of moh_2 =", end='')
-print(moh_2)
+print_type_value(moh_2, "moh_2")
 
 # moh_3 is an int, but Hex
 moh_3 = 0x23
-# print type of moh_3
-print("type of moh_3 = ", end='')
-print(type(moh_3))
-# print value of moh_3,base 10 value not Hex value
-print("value of moh_3 =", end='')
-print(moh_3)
+print_type_value(moh_3, "moh_3")
 
 #####
 # in int:
@@ -66,12 +44,7 @@ print(moh_3)
 
 # moh_4 is a float
 moh_4 = 3.4343e20
-# print type of moh_4
-print("type of moh_4 = ", end='')
-print(type(moh_4))
-# print value of moh_4
-print("value of moh_4 =", end='')
-print(moh_4)
+print_type_value(moh_4, "moh_4")
 
 ######
 # list
@@ -79,20 +52,20 @@ print(moh_4)
 
 # list_1 is a list
 list_1 = list()
-# print type of list_1
-print("type of list_1 = ", end='')
-print(type(list_1))
-# print value of list_1
-print("value of list_1 =", end='')
-print(list_1, end='\n\n')
+print_type_value(list_1, "list_1")
 
 # list_2 is a list
 list_2 = ['red', 54, 3.43e23]
-# print type of list_2
-print("type of list_2 = ", end='')
-print(type(list_2))
-# print value of list_2
-print("value of list_2 =", end='')
-print(list_2, end='\n\n')
+print_type_value(list_2, "list_2")
 
+#####
+# tuple
+# tuple is immutable version of sequence
 
+# tuple_1 is a tuple
+tuple_1 = (3, 4, 3)
+print_type_value(tuple_1, "tuple_1")
+
+# tuple_2 is a tuple
+tuple_2 = (3,)
+print_type_value(tuple_2, "tuple_2")
