0e8b19959d4396acd67069638e4e59033d34c1fe
moh <dabiri1377@gmail.com>
2018-02-18 07:40:43 +0330

    complete 1.4_test.py

---
+++ Chapter_1/1.4_test.py
@@ -0,0 +1,23 @@
+# Control flow
+
+####
+# Conditionals
+
+# if first_condition :
+#   first_body
+# elif second_condition:
+#   second_condition
+# elif third_condition:
+#   third_body
+# else:
+#   fourth_body
+if 1 == 2:
+    print("WTF!! 1 is not equal to 2!!")
+elif 2 == 3:
+    print("wo wo wo WTF?!! 2 == 3?")
+elif 3 == 4:
+    print("what are you doing man? 3 is 4?")
+else:
+    print("non of your shite condition is true!!!")
+
+
