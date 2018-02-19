258740098e3c38dad880f6c01e267cbf93d7b074
moh <dabiri1377@gmail.com>
2018-02-19 08:30:43 +0330

    add while and for into 1.4.2_test.py

---
+++ Chapter_1/1.4.2_test.py
@@ -0,0 +1,25 @@
+# Control flow
+
+####
+# Loops
+
+# while loops:
+# while condition:
+#   body
+i = 7
+while 0 < i:
+    print(i)
+    i -= 1
+
+print("end of while")
+
+# for loops:
+# for element in iterable:
+#   body
+for i in range(5):
+    print(i)
+
+print("end of for")
+
+
+
