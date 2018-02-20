21ef44b7e3620570cd262a4adfebf046bc2f51d5
moh <dabiri1377@gmail.com>
2018-02-20 15:50:11 +0330

    edit time_calculator_for_insertion_sort body and add some test case

--- for_fun/insertion_sort_analizer.py
+++ for_fun/insertion_sort_analizer.py
@@ -1,5 +1,6 @@
 import time
 
+
 def insertion_sort(input_list):
     """
     Insertion sort algorithm.
@@ -27,12 +28,10 @@ def random_list_generator(len_of_list):
 
 
 def time_calculator_for_insertion_sort(input_list_for_insertion_sort):
-
     first_millis = int(round(time.time() * 1000))
     insertion_sort(input_list_for_insertion_sort)
     last_millis = int(round(time.time() * 1000))
-    time_pass = last_millis - first_millis
-    return time_pass
+    return last_millis - first_millis
 
 
 def all_list_analyzer(len_of_list):
@@ -44,4 +43,7 @@ def random_list_analyzer(len_of_list):
 
 
 # __main__
+
+test_list = [x for x in range(5000,0,-1)]
+print(time_calculator_for_insertion_sort(test_list))
 
