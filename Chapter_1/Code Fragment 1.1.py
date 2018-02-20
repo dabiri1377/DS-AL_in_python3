1e23f3d802f2916511738bb90ecb0e4fab65770c
moh <dabiri1377@gmail.com>
2018-02-20 11:22:32 +0330

    fix some warning

--- Chapter_1/Code Fragment 1.1.py
+++ Chapter_1/Code Fragment 1.1.py
@@ -3,7 +3,7 @@ print('Please enter all your letter grades, one per line.')
 print('Enter a blank line to designate the end.')
 # map from letter grade to point value
 points = {'A+': 4.0, 'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.67,
-          'C+': 2.33, 'C': 2.0, 'C': 1.67, 'D+': 1.33, 'D': 1.0, 'F': 0.0}
+          'C+': 2.33, 'C': 2.0, 'C-': 1.67, 'D+': 1.33, 'D': 1.0, 'F': 0.0}
 num_courses = 0
 total_points = 0
 done = False
