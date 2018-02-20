34aad4737d6f14c2fbcad812baeec6df08abd149
moh <dabiri1377@gmail.com>
2018-02-20 15:36:15 +0330

    add func. header

---
+++ Chapter_1/1.5_test.py
@@ -0,0 +1,10 @@
+####
+# functions
+
+####
+# common built-in functions
+
+# abs() => Return the absolute value of a number
+print("abs 2=:", abs(2))
+print("abs -2=:", abs(-2))
+
---
+++ for_fun/insertion_sort_analizer.py
@@ -0,0 +1,40 @@
+def insertion_sort(input_list):
+    """
+    Insertion sort algorithm.
+
+    :param input_list:
+     List for sort
+    :return:
+     Sorted input_list
+    """
+    for index in range(1, len(input_list)):
+
+        current_value = input_list[index]
+        position = index
+
+        while position > 0 and input_list[position - 1] > current_value:
+            input_list[position] = input_list[position - 1]
+            position = position - 1
+
+        input_list[position] = current_value
+    return input_list
+
+
+def random_list_generator(len_of_list):
+    pass
+
+
+def time_calculator():
+    pass
+
+
+def all_list_analyzer(len_of_list):
+    pass
+
+
+def random_list_analyzer(len_of_list):
+    pass
+
+
+# __main__
+
